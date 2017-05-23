3463dda7a3bd4b076ff7d2b5b9ce9dae06ca03db
The-BFG <guerzonigiacom95@gmail.com>
2017-05-23 17:55:12 +0200

    Perfectioned al the available feature and deleted the read from file of the queries

--- src/FastFoodManager/FastFoodManager.java
+++ src/FastFoodManager/FastFoodManager.java
@@ -16,6 +16,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -67,12 +68,12 @@ public class FastFoodManager {
      * @return
      */
     public static ArrayList<String> readQuery (Integer choosed, String file){
-        ArrayList<String> query = new ArrayList();
+        ArrayList<String> query = new ArrayList<String>();
         
         try{
             BufferedReader buff;
             buff=new BufferedReader(new FileReader(file));
-            String row = "";
+            String row;
             //System.out.println("--" + choosed.toString());
             
             while((row=buff.readLine()) != null ) {
@@ -111,7 +112,6 @@ public class FastFoodManager {
                 "2)Aggiornamento di dati gia esistenti;\n"+
                 "3)Interrogazioni di default;\n"+
                 "0)Esci.";
-                
         String insertMenu = "Inserisci il numero dell'operazione di inserimento da eseguire:\n"+
                 "1)Inserisci dati di un nuovo cliente;\n"+
                 "2)Inserisci un nuovo ordine per un dato cliente;\n"+
@@ -119,32 +119,31 @@ public class FastFoodManager {
                 "4)Aggiungi un cliente ai clienti fedeli.\n"+
                 "5)Apertura nuovo stabilimento\n"+
                 "0)Torna al menu precedente.";
-        
         String updateMenu = "Inserisci il numero dell'operazione di aggiornamento da eseguire:\n"+
                 "1)Aggiorna il prezzo di un prodotto venduto da un determinato fornitore;\n"+
                 "2)Aggiorna il prezzo di un alimento presente nel menu di uno stabilimento;\n"+
                 "0)Tornaal menu precedente.";
-        
         String queryMenu = "Inserisci il numero dell'operazione di visualizzazione da eseguire:\n"+
                 "1)Mostra il menu degli alimenti e il menu delle bevande di uno stabilimento;\n"+
                 "2)Mostra storico degli ordini di un cliente in ordine cronologico;\n"+
                 "0)Torna al menu precedente.";
-        
         Scanner in = new Scanner(System.in);
-        
+        Connection conn = null;
+        PreparedStatement ps = null;
+        Statement stmt = null;
+        CallableStatement cStmt = null;
+        ResultSet rs;
         //Trying loading driver to access postgresql database
         try{        
 
             Class.forName ("org.postgresql.Driver");
         }
         catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }  
         
-        
         //Database connection and check user dataprogramma
-        boolean dbconn= false;;
-        Connection conn = null;
+        boolean dbconn= false;
         do {
             try{
                 System.out.println("Inserisci il nome utente amministratore postgres:");
@@ -158,111 +157,123 @@ public class FastFoodManager {
             }
         }while(dbconn);
         
-        
         //Menu;
-        int choosedG;
-        PreparedStatement ps = null;
-        Statement stmt = null;
-        CallableStatement cStmt = null;
-        ResultSet rs = null;
-        String dir = System.getProperty("user.dir");
-        //System.out.println(dir);
-        do{
-            System.out.println("\n");
-            System.out.println(genericMenu);
-            choosedG = in.nextInt();
-            try{
-                switch(choosedG){
-                    case 0:
-                        break;
-                    case 1:
-                        System.out.println(insertMenu);
-                        int choosedI = in.nextInt();
-                        ArrayList<String> query;
-                        switch(choosedI){
-                            case 1:
-                                query = readQuery(1, "../insert.sql");
-                                //System.out.println(query.get(0)+"\n");
-                                ps = conn.prepareStatement(query.get(0));
-                                System.out.println("Inserisci il codice fiscale del nuovo cliente");
-                                String cf = in.nextLine();
-                                cf = in.nextLine();
-                                ps.setString(1,cf);
-                                System.out.println("Inserisci il nome:");
-                                ps.setString(2,in.nextLine());
-                                System.out.println("Inserisci il cognome:");
-                                ps.setString(3,in.nextLine());
-                                System.out.println("Inserisci l'indirizzo di residenza:");
-                                ps.setString(4,in.nextLine());
-                                System.out.println("Inserisci la città di residenza:");
-                                ps.setString(5,in.nextLine());
-                                ps.executeUpdate();
-
-                                //System.out.println(query.get(1)+"\n");
-                                ps = conn.prepareStatement(query.get(1));
-                                ps.setString(1,cf);
-                                System.out.println("Inserisci la mail del cliente:");
-                                ps.setString(2,in.nextLine());
-                                ps.setString(3,null);
-                                ps.executeUpdate();
-                                System.out.println("Inserimento avvenuto con successo.");
-                                break;
-                            case 2:
-                                
-                        }
-                        break;
-                    case 2:
-                        System.out.println(updateMenu);
-                        int choosedU = in.nextInt();
-                        switch(choosedU){
-                            case 1:
-                                break;
-                        }
-                        break;
-                    case 3:
-                        System.out.println(queryMenu);
-                        int choosedQ = in.nextInt();
-                        switch(choosedQ){
-                            case 1:
-                                query = readQuery(1, "../query.sql");
-                                stmt = conn.createStatement();
-                                System.out.println("Inserisci il nome dello stabilimento di cui vuoi visualizzare il menu scegliendo tra i seguenti:");
-                                rs = stmt.executeQuery(query.get(0));
-                                ArrayList<String> stabElenco = new ArrayList();
-                                while(rs.next()){
-                                    stabElenco.add(rs.getString(1));
-                                    System.out.printf("%-30s %-30s %-30s\n",rs.getString(1), rs.getString(2), rs.getString(3));
-                                }
-                                System.out.println("\nNome stabilimento scelto?");
-                                in.nextLine();
-                                ps = conn.prepareStatement(query.get(1));
-                                String stab = in.nextLine();
-                                if(stabElenco.contains(stab)){
-                                    ps.setString(1,stab);
-                                    rs = ps.executeQuery();
-                                    System.out.printf("\nListino cibi:\n%-30s %-30s\n","Nome cibo","Prezzo");
-                                    while(rs.next()){
-                                        System.out.printf("%-30s %-30s\n",rs.getString(1),  rs.getInt(2));
-                                    } 
-                                    ps = conn.prepareStatement(query.get(2));
-                                    ps.setString(1,stab);
-                                    rs = ps.executeQuery();                                
-                                    System.out.printf("\nListino bevande:\n%-30s %-30s\n","Nome bevanda","Prezzo");
-                                    while(rs.next()){
-                                        System.out.printf("%-30s %-30s\n",rs.getString(1),  rs.getInt(2));
+        int choosedG=-1;
+        //System.out.println(System.getProperty("user.dir"));
+        if(conn!=null){
+            do{
+                do{
+                    System.out.println("\n");
+                    System.out.println(genericMenu);
+                    try{
+                        choosedG = in.nextInt();
+                    }catch(InputMismatchException imsm){
+                        System.out.println("Scelta non ammessa");
+                        in.nextLine();
+                    }
+                }while(choosedG == -1);
+                try{
+                    switch(choosedG){
+                        case 0:
+                            break;
+                        case 1:
+                            System.out.println(insertMenu);
+                            int choosedI = in.nextInt();
+                            ArrayList<String> query;
+                            switch(choosedI){
+                                case 1:
+                                    query = readQuery(1, "../insert.sql");
+                                    ps = conn.prepareStatement("INSERT INTO PERSONA(cf,nome,cognome,indirizzo_residenza,citta_residenza) VALUES (?,?,?,?,?);");
+                                    System.out.println("Inserisci il codice fiscale del nuovo cliente");
+                                    in.nextLine();
+                                    String cf = in.nextLine();
+                                    System.out.println(cf.toUpperCase());
+                                    ps.setString(1,cf);
+                                    System.out.println("Inserisci il nome:");
+                                    ps.setString(2,in.nextLine());
+                                    System.out.println("Inserisci il cognome:");
+                                    ps.setString(3,in.nextLine());
+                                    System.out.println("Inserisci l'indirizzo di residenza:");
+                                    ps.setString(4,in.nextLine());
+                                    System.out.println("Inserisci la città di residenza:");
+                                    ps.setString(5,in.nextLine());
+                                    try{
+                                        ps.executeUpdate();
+                                    }catch(SQLException e){
+                                        System.err.println(e.getMessage());
+                                    }finally{
+                                        ps = conn.prepareStatement("INSERT INTO CLIENTE(cf,email,numero_carta) VALUES (?,?,?);");
+                                        ps.setString(1,cf.toUpperCase());
+                                        System.out.println("Inserisci la mail del cliente:");
+                                        ps.setString(2,in.nextLine());
+                                        ps.setString(3,null);
+                                        ps.executeUpdate();
+                                        System.out.println("Inserimento avvenuto con successo.");
                                     }
-                                }else
-                                    System.out.println("\nNome stabilimento non presente in elenco, verificare di aver inserito il nome corretto(comprese maiuscole)");
-                                break;
-                        }
-                        break;
-                    default:
-                        System.out.println("Opzione non valida.");
+                                    break;
+                                case 2:
+
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            System.out.println(updateMenu);
+                            int choosedU = in.nextInt();
+                            switch(choosedU){
+                                case 1:
+                                    break;
+                            }
+                            break;
+                        case 3:
+                            System.out.println(queryMenu);
+                            int choosedQ = in.nextInt();
+                            switch(choosedQ){
+                                case 1:
+                                    query = readQuery(1, "../query.sql");
+                                    stmt = conn.createStatement();
+                                    System.out.println("Inserisci il nome dello stabilimento di cui vuoi visualizzare il menu scegliendo tra i seguenti:");
+                                    //rs = stmt.executeQuery(query.get(0));
+                                    rs = stmt.executeQuery("SELECT nome, citta, indirizzo FROM stabilimento;");
+                                    ArrayList<String> stabElenco = new ArrayList<String>();
+                                    while(rs.next()){//rs.close();
+                                        stabElenco.add(rs.getString(1));
+                                        System.out.printf("%-30s %-30s %-30s\n",rs.getString(1), rs.getString(2), rs.getString(3));
+                                    }
+                                    System.out.println("\nNome stabilimento scelto?");
+                                    in.nextLine();
+                                    String stab = in.nextLine();
+                                    if(stabElenco.contains(stab)){
+                                        ps = conn.prepareStatement( "SELECT c.nome_cibo, c.prezzo "+
+                                                                "FROM listino_cibo AS c, stabilimento AS s "+
+                                                                "WHERE c.nome_stabilimento=s.nome AND c.nome_stabilimento=?;");
+                                        ps.setString(1,stab);
+                                        rs = ps.executeQuery();
+                                        System.out.printf("\nListino cibi per lo stabilimento %s:\n%-30s %-30s\n",stab,"Nome cibo","Prezzo");
+                                        while(rs.next()){
+                                            System.out.printf("%-30s %-30s\n",rs.getString(1),  rs.getInt(2));
+                                        } 
+                                        //ps = conn.prepareStatement(query.get(2));
+                                        ps = conn.prepareStatement("SELECT b.nome_bevanda, b.prezzo "+
+                                                                    "FROM listino_bevande AS b, stabilimento AS s "+
+                                                                    "WHERE b.nome_stabilimento=s.nome AND b.nome_stabilimento=?;");
+                                        ps.setString(1,stab);
+                                        rs = ps.executeQuery();                                
+                                        System.out.printf("\nListino bevande:\n%-30s %-30s\n","Nome bevanda","Prezzo");
+                                        while(rs.next()){
+                                            System.out.printf("%-30s %-30s\n",rs.getString(1),  rs.getInt(2));
+                                        }
+                                    }else
+                                        System.out.println("\nNome stabilimento non presente in elenco, verificare di aver inserito il nome corretto(comprese maiuscole)");
+                                    break;
+                            }
+                            break;
+                        default:
+                            System.out.println("Opzione non valida.");
+                    }
+                }catch(SQLException e){
+                    System.err.println(e.getMessage());
                 }
-            }
-            catch(SQLException e){
-                System.err.println(e.getMessage());
-            }
+            
             
             /*
                             ps= conn.prepareStatement(sql);
@@ -274,13 +285,17 @@ public class FastFoodManager {
             cStmt = conn.prepareCall(query.get(1).replace(';', ' '));
                                 cStmt.setString(1, in.nextLine());
                                 rs = cStmt.executeQuery();*/
-        }while(choosedG != 0);
-        
+            }while(choosedG != 0);
+        }
         try{
-            
-            ps.close();
-            stmt.close();
-            conn.close();
+            if(ps!=null)
+                ps.close();
+            if(stmt!=null)
+                stmt.close();
+            if(ps!=null)
+                ps.close();
+            if(conn!=null)
+                conn.close();
         }
         catch(SQLException e){
             System.err.println(e.getMessage());
